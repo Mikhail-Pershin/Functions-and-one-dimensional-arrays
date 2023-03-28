3d422fbf95d8fa07bc0aa1193a5ce0775922da45
Mikhail_Pershin <mcpershin@yandex.ru>
2023-03-28 11:02:31 +0300

    add solution of task 3

--- Program.cs
+++ Program.cs
@@ -40,41 +40,66 @@
 
 
 
-// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+// // Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
 
-// [3, 7, 23, 12] -> 19
+// // [3, 7, 23, 12] -> 19
 
-// [-4, -6, 89, 6] -> 0
+// // [-4, -6, 89, 6] -> 0
 
+
+// Console.Write($"Введите размер массива: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] array = GetArray(num);
+// Console.WriteLine($"Одномерный массив, заполненный случайными числами: ");
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+// Console.WriteLine($"Сумма элементов, стоящих на нечётных индексах -> {SumNumbersOddIndexes(array)}");
+
+// // -------------------------------Общий метод-------------------------------------------
+
+// int[] GetArray(int size)
+// {
+//   int[] result = new int[size];
+//   for (int i = 0; i < size; i++)
+//   {
+//     result[i] = new Random().Next(-10, 20);
+//   }
+//   return result;
+
+
+//   // -------------------------------Общий метод-------------------------------------------
+
+// }
+// int SumNumbersOddIndexes(int[] array)
+// {
+//   int sum = 0;
+//   for (int i = 1; i < array.Length; i += 2)
+//   {
+//     sum += array[i];
+//   }
+//   return sum;
+// }
+
+
+
+// Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3, 7.4, 22.3, 2, 78] -> 76
 
 Console.Write($"Введите размер массива: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int[] array = GetArray(num);
-Console.WriteLine($"Одномерный массив, заполненный случайными числами: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+int i_min = 0;
+int i_max = 0;
+
+
+for (int i = 0; i < n; i++)
+{
+  int num = Convert.ToInt32(new Random().NextDouble() * 10);
+  array[i] = num;
+  if (array[i] < array[i_min]) i_min = i;
+  if (array[i] > array[i_max]) i_max = i;
+}
+
+Console.WriteLine($"Массив чисел: ");
 Console.WriteLine($"[{string.Join(", ", array)}]");
-Console.WriteLine($"Сумма элементов, стоящих на нечётных индексах -> {SumNumbersOddIndexes(array)}");
-
-// -------------------------------Общий метод-------------------------------------------
-
-int[] GetArray(int size)
-{
-  int[] result = new int[size];
-  for (int i = 0; i < size; i++)
-  {
-    result[i] = new Random().Next(-10, 20);
-  }
-  return result;
-
-
-  // -------------------------------Общий метод-------------------------------------------
-
-}
-int SumNumbersOddIndexes(int[] array)
-{
-  int sum = 0;
-  for (int i = 1; i < array.Length; i += 2)
-  {
-    sum += array[i];
-  }
-  return sum;
-}
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива -> {(array[i_max] - array[i_min])}");
